db7a90cc1eb5e876d64b8eaa17e7183c34a0f71d
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-01 14:50:38 -0800

    Define schema

--- database/index.js
+++ database/index.js
@@ -2,15 +2,23 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  // Declare schema
+  github_handle: String,
+  repo_name: String,
+  repo_url: String,
+  description: String,
+  created_at: String,
+  updated_at: String,
+  forks: Number,
+  watchers: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (err, repos) => {
+  // save a repo or repos to mongodb
+  if(err) console.error(err);
+  console.log(repos)
 }
 
 module.exports.save = save;
