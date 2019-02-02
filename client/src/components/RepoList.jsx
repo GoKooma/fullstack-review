e760774dd177b1b04e49725df12f0833045b779b
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-02 15:46:55 -0800

    Render the github handle of the owner once username is defined

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const RepoList = (props) => (
-  <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
-  </div>
-)
+const RepoList = (props) => {
+  if (props.user.length !== 0) {
+    return (
+      <div>
+        <h4> Repo List Component </h4>
+        There are {props.repos.length} repos for {props.user}.
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h4> Repo List Component </h4>
+        There are {props.repos.length} repos.
+      </div>
+    )
+  }
+  
+}
 
 export default RepoList;
