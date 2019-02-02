a94e15f4bcc237256ee47ffbb100e6a1b86c784a
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-02 15:48:58 -0800

    Call GET and POST ajax requests to the server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,32 +3,60 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      repos: []
+      repos: [],
+      username: ''
     }
 
+    this.refresh = this.refresh.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.state.username.length !== 0) {
+      this.refresh(this.state.username);
+    }
+  }
+
+  refresh(term) {
+    axios
+      .get(`/repos/${term}`, { params: { owner: term }})
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          repos: data.data,
+          username: data.data[0].owner
+        })
+        console.log(this.state.repos)
+      })
+      .catch((err) => {
+        console.error(err);
+      })
   }
 
   search (term) {
     console.log(`${term} was searched`);
     // Send ajax request to the server
-    $.ajax({
-      method: "POST",
-      url: "http://127.0.0.1:1128/repos",
-      data: { username: term },
-      contentType: '/application/json'
+    this.setState({
+      username: term
     })
+    axios
+      .post(`/repos/${term}`)
+      .then(() => this.refresh(term))
+      .catch((err) => console.error(err));
   }
 
   render () {
-    return (<div>
+    return (
+    <div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
+      <RepoList repos={this.state.repos} user={this.state.username}/>
+      <Search onSearch={this.search.bind(this)} repos={this.state.repos}/>
     </div>)
   }
 }
